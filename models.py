9b5ddf05d73c4ab9d855fc1097e8ffb79e7339a3
Precious Ahaneku (student) <335171@edu.mkcollege.ac.uk>
2025-12-01 13:46:23 +0000

    first commit

--- models.py
+++ models.py
@@ -2,3 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+#User model for the database
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(40), unique=True, nullable=False)
+    lastname = db.Column(db.String(40), unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String(255), unique=True, nullable=False)
+    dob = db.Column(db.Date, nullable=False)
+    user_option = db.Column(db.String, nullable=False)
+
+    def __repr__(self): 
+        return f'<User {self.firstname}>'
